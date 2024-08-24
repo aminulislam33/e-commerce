beb2c0ac3a7b7a90b63e8653985801bea4922a09
Aminul Islam <alfaz1873@gmail.com>
2024-08-24 22:52:22 +0530

    bug fix about token

--- server/user-mgmt/utils/verifyToken.js
+++ server/user-mgmt/utils/verifyToken.js
@@ -12,10 +12,9 @@ function verifyToken(req, res, next) {
     try {
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
         req.user = decoded.user;
-        console.log("req.user is: ", req.user);
         next();
     } catch (err) {
-        res.status(401).json({ msg: 'Token is not valid' });
+        res.status(401).json({ msg: 'Token is not valid. Please log in again.' });
     }
 }
 
